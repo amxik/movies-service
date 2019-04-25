0d903e414da78f322e9fe2811e1e50538798ad58
Maksym Niskov <amxxxik@gmail.com>
2019-04-25 21:41:10 +0300

    add method POST for movies

--- src/main/java/me/max/moviesservice/controllers/MovieEntityController.java
+++ src/main/java/me/max/moviesservice/controllers/MovieEntityController.java
@@ -4,8 +4,10 @@ package me.max.moviesservice.controllers;
 import me.max.moviesservice.movie.MovieEntity;
 import me.max.moviesservice.service.MovieEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -24,6 +26,14 @@ public class MovieEntityController {
     public @ResponseBody List<MovieEntity> getMovies(@RequestParam(value = "offset",required = false,defaultValue = "0") int offset,
                                                      @RequestParam(value = "limit",required = false,defaultValue = "20") int limit){
        return movieEntityService.getAllMovies(offset,limit);
+ }
+
+
+@RequestMapping(method = RequestMethod.POST,path = "movies",
+                produces = {"application/json","application/xml"})
+    @ResponseStatus(code = HttpStatus.CREATED)
+ public @ResponseBody MovieEntity create(@Valid @RequestBody MovieEntity movieToCreate){
+     return movieEntityService.createMovie(movieToCreate);
  }
 
 
