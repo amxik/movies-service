143f4d8108c160845e9780c3dab79e862c0c1d42
Maksym Niskov <amxxxik@gmail.com>
2019-04-25 22:04:47 +0300

    add method GET for one movie add method PUT for movies

--- src/main/java/me/max/moviesservice/controllers/MovieEntityController.java
+++ src/main/java/me/max/moviesservice/controllers/MovieEntityController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -32,9 +31,36 @@ public class MovieEntityController {
 @RequestMapping(method = RequestMethod.POST,path = "movies",
                 produces = {"application/json","application/xml"})
     @ResponseStatus(code = HttpStatus.CREATED)
- public @ResponseBody MovieEntity create(@Valid @RequestBody MovieEntity movieToCreate){
+ public @ResponseBody MovieEntity createMovie(@RequestBody MovieEntity movieToCreate){
      return movieEntityService.createMovie(movieToCreate);
  }
+
+    @RequestMapping(method = RequestMethod.GET, path = "movies/{id}",
+            produces = {"application/json", "application/xml"})
+    public @ResponseBody MovieEntity getMovie(@PathVariable("id") long movieId) {
+        return movieEntityService.getMovieById(movieId);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "movies/{id}",
+                    produces = {"application/json","application/xml"})
+    public @ResponseBody
+    MovieEntity replaceMovie(@PathVariable("id") long movieId,
+                             @RequestBody MovieEntity newMovie){
+     MovieEntity movieEntity = new MovieEntity();
+     movieEntity.setId(movieId);
+     movieEntity.setTitle(newMovie.getTitle());
+     movieEntity.setGenre(newMovie.getGenre());
+     movieEntity.setDescription(newMovie.getDescription());
+     movieEntity.setDuration(newMovie.getDuration());
+     movieEntity.setReleaseDate(newMovie.getReleaseDate());
+
+     return movieEntityService.updateMovie(movieEntity);
+ }
+
+
+ 
+
+
 
 
 }
